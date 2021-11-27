a83e4b0cc7d3c8fd837780d347cb736f68ee7ecf
ArubianLady85 <88424294+ArubianLady85@users.noreply.github.com>
2021-11-27 11:17:17 +0100

    Add files via upload

---
+++ exercise 2/pub.js
@@ -0,0 +1,39 @@
+const age = 18;
+
+if (age > 18) {
+
+    document.write("Enjoy your night inside!");
+
+} else if (age < 18) {
+
+    document.write("You're to young for this Pub!")
+
+} else if (age == 18) {
+
+    document.write("Enjoy your night inside!")
+
+}
+
+const isFemale = true;
+
+if (isFemale) {
+
+    document.write("You're Ready 4 Ladies Night!!")
+
+} else {
+
+    document.write("No Enter! You're not a Lady!!")
+
+}
+
+const driverStatus = false;
+
+if (driverStatus) {
+
+    document.write("You are allowed to drive")
+
+} else {
+
+    document.write("You are not allowed to drive")
+
+}
